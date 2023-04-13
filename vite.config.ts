db68dfb0490a6191a2f3b89a699d6c413bd4bc7c
Mike Lee <mike.lee816@gmail.com>
2023-04-13 19:44:41 -0400

    Install and set up vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteSvgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSvgr()],
   test: {
     globals: true,
     environment: 'jsdom',
