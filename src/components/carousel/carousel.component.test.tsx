99249227ab6bf9d662176f3125fec4e400ea58e8
Mike Lee <mike.lee816@gmail.com>
2023-04-13 15:56:26 -0400

    Add tests for when Carousel receives prev and next props
    prev and next props will display the previous and next items

--- src/components/carousel/carousel.component.test.tsx
+++ src/components/carousel/carousel.component.test.tsx
@@ -6,10 +6,10 @@ const items: React.ReactElement[] = [
     <div key={1}>Item One</div>,
     <div key={2}>Item Two</div>,
     <div key={3}>Item Three</div>,
-    <div key={4}>Item Four</div>,
+    <div key={4}>Item Four</div>
 ];
 
-describe('Carousel tests', () => {
+describe('Carousel tests without prev and next items', () => {
     beforeEach(() => {
         render(<Carousel items={items} />);
     });
@@ -78,5 +78,104 @@ describe('Carousel tests', () => {
         }
 
         expect(nextButton).toBeDisabled();
+    });
+});
+
+describe('Carousel tests with prev and next items' , () => {
+    beforeEach(() => {
+        render(<Carousel items={items} prev next />);
+    });
+
+    it('should render the first three items', async () => {
+        const user = userEvent.setup();
+
+        // initally there is no previous item, so the next button needs to be clicked
+        const nextButton = screen.getByRole('button', { name: 'Next' });
+        await user.click(nextButton);
+
+        const text1 = items[0].props.children;
+        const text2 = items[1].props.children;
+        const text3 = items[2].props.children;
+        
+        const firstItem = screen.getByText(text1);
+        const secondItem = screen.getByText(text2);
+        const thirdItem = screen.getByText(text3);
+    
+        expect(firstItem).toBeInTheDocument();
+        expect(secondItem).toBeInTheDocument();
+        expect(thirdItem).toBeInTheDocument();
+    });
+
+    it('should shift the displayed items one to the right (drop the previous item and bring in the next item)', async () => {
+        const user = userEvent.setup();
+
+        // initally there is no previous item, so the next button needs to be clicked
+        const nextButton = screen.getByRole('button', { name: 'Next' });
+        await user.click(nextButton);
+        
+        const text1 = items[0].props.children;
+        const text2 = items[1].props.children;
+        const text3 = items[2].props.children;
+        const text4 = items[2].props.children;
+        
+        await user.click(nextButton);
+
+        const firstItem = screen.queryByText(text1);
+        const secondItem = screen.getByText(text2);
+        const thirdItem = screen.getByText(text3);
+        const fourthItem = screen.getByText(text4);
+    
+        expect(firstItem).not.toBeInTheDocument();
+        expect(secondItem).toBeInTheDocument();
+        expect(thirdItem).toBeInTheDocument();
+        expect(fourthItem).toBeInTheDocument();
+    });
+
+    it('should shift the displayed items one to the left (bring in the previous previous item and drop the next item)', async () => {
+        const user = userEvent.setup();
+
+        const nextButton = screen.getByRole('button', { name: 'Next' });
+        await user.click(nextButton);
+        await user.click(nextButton);
+        
+        const prevButton = screen.getByRole('button', { name: 'Previous' });
+        await user.click(prevButton);
+        
+        const text1 = items[0].props.children;
+        const text2 = items[1].props.children;
+        const text3 = items[2].props.children;
+        const text4 = items[3].props.children;
+
+        const firstItem = screen.getByText(text1);
+        const secondItem = screen.getByText(text2);
+        const thirdItem = screen.getByText(text3);
+        const fourthItem = screen.queryByText(text4);
+
+        expect(firstItem).toBeInTheDocument();
+        expect(secondItem).toBeInTheDocument();
+        expect(thirdItem).toBeInTheDocument();
+        expect(fourthItem).not.toBeInTheDocument();
+        
+    });
+
+    it('should display an empty placeholder for the previous item', () => {
+        const placeholder = screen.getByTestId('prev-placeholder');
+
+        expect(placeholder).toBeInTheDocument();
+    });
+
+    it('should display an empty placeholder for the next item', async () => {
+        const user = userEvent.setup();
+        
+        const nextButton = screen.getByRole('button', { name: 'Next' });
+        const amountNextItems = items.length - 1;
+        
+        for (let i = 1; i <= amountNextItems; i++) {
+            await user.click(nextButton);
+        }
+
+        const placeholder = screen.getByTestId('next-placeholder');
+
+        expect(placeholder).toBeInTheDocument();
     });
 });
