0cca0247924035f6fb2a2eb1dfb45d254fc1615f
Mike Lee <mike.lee816@gmail.com>
2023-04-13 16:05:05 -0400

    Add option to display the previous and\or next items

--- src/components/carousel/carousel.component.tsx
+++ src/components/carousel/carousel.component.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 
 import './carousel.styles.scss';
 
 interface Props {
-    items: any[]
+    items: any[],
+    prev?: boolean
+    next?: boolean
 }
 
-const Carousel: React.FC<Props> = ({ items }) => {
+const Carousel: React.FC<Props> = ({ items, prev, next }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [itemWidth, setItemWidth] = useState(0);
+
+    useLayoutEffect(() => {
+        const width = document?.getElementById('item-cur')?.offsetWidth;
+        
+        if (width) setItemWidth(width);
+    }, [items]);
 
     return (
         <section className='carousel'>
@@ -15,9 +24,33 @@ const Carousel: React.FC<Props> = ({ items }) => {
                 onClick={() => setCurrentIndex(currentIndex - 1)}
                 disabled={currentIndex <= 0}
                 >Previous</button>
-            <div className='item'>
+                {
+                    prev
+                    ?
+                        <div className='item item-prev'>
+                            {
+                                currentIndex - 1 >= 0
+                                ? items[currentIndex - 1]
+                                : <div style={{ width: itemWidth }} data-testid='prev-placeholder'></div>
+                            }
+                        </div>
+                    : null
+                }
+            <div id='item-cur' className='item'>
                 {items[currentIndex]}
             </div>
+            {
+                next
+                ?
+                    <div className='item item-next'>
+                        {
+                            currentIndex + 1 <= items.length - 1
+                            ? items[currentIndex + 1]
+                            : <div style={{ width: itemWidth }} data-testid='next-placeholder'></div>
+                        }
+                    </div>
+                : null
+            }
             <button
                 onClick={() => setCurrentIndex(currentIndex + 1)}
                 disabled={currentIndex >= items.length - 1}
