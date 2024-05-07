34e5d3c34a625482e755e8512cd4de66cf0a7d93
Mike Lee <mike.lee816@gmail.com>
2024-05-07 18:48:35 -0400

    Update react-flip-toolkit
    Updating react-flip-toolkit caused the error "window.matchMedia is not a function" in tests that use react-flip-tool kit (Carousel using MultipleItems components). I defined window.matchMedia() in the test setup file to fix this.

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,20 @@
 import '@testing-library/jest-dom'
+
+/*
+    - Fixes "window.matchMedia is not a function" error in Carousel tests that use <MultipleItems /> with react-flip-toolkit.
+    - The error only happened with components that used react-flip-toolkit
+    - Documentation found here: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+*/
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  })
